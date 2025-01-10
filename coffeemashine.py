c7ebd0d4cb8b2fa7c79805b5d8d5c140fdcdeb6a
Viktoriia Kazniienko <nk.akk16@gmail.com>
2025-01-10 22:37:43 +0200

    add new functions for understanding code

--- coffeemashine.py
+++ coffeemashine.py
@@ -9,19 +9,45 @@ from resources import resources
 
 def ver_transaction_espresso(cost_coffee):
     enough_money = 1.5
-    if cost_coffee < enough_money:
+    if cost_coffee<enough_money:
         print("Sorry that's not enough money. Money refunded.")
-    elif cost_coffee >= enough_money:
-        money_in_change = cost_coffee - enough_money
-        print(f"Here is ${money_in_change}")
+    elif cost_coffee>=enough_money:
+
+        money_in_change=cost_coffee-enough_money
+        rest=round(money_in_change,1)
+        print(f"Here is in change ${rest}")
         print("Here is your espresso ☕️. Enjoy!")
+
+
+def ver_transaction_latte(cost_coffee):
+    enough_money = 2.5
+    if cost_coffee<enough_money:
+        print("Sorry that's not enough money. Money refunded.")
+    elif cost_coffee>=enough_money:
+
+        money_in_change=cost_coffee-enough_money
+        rest=round(money_in_change,1)
+        print(f"Here is in change ${rest}")
+        print("Here is your latte☕️. Enjoy!")
+
+
+def ver_transaction_cappuccino(cost_coffee):
+    enough_money=3.0
+    if cost_coffee<enough_money:
+        print("Sorry that's not enough money. Money refunded.")
+    elif cost_coffee>=enough_money:
+
+        money_in_change=cost_coffee-enough_money
+        rest=round(money_in_change,1)
+        print(f"Here is in change ${rest}")
+        print("Here is your cappuccino ☕️. Enjoy!")
 
 
 # TODO-5: Process Coins:
 #    Create a function to handle coin input from the user.
 #    Calculate the total value of the inserted coins.
 
-def count():
+def count(choose):
     print("Please insert coins")
 
     quarters = int(input("how many quarters?:"))
@@ -37,50 +63,52 @@ def count():
     cost_coffee = (quarters_sum + dimes_sum + nickles_sum + pennies_sum) / 100
     print(cost_coffee)
 
-    ver_transaction_espresso(cost_coffee)
+    if choose=="espresso":
+        ver_transaction_espresso(cost_coffee)
+    elif choose=="latte":
+        ver_transaction_latte(cost_coffee)
 
 
-# TODO-1: Prompt User for Action:
+#TODO-1: Prompt User for Action:
 #     Implement a function to ask the user: "What would you like? (espresso/latte/cappuccino):".
 #     Include options for "report" to show the status and "off" to turn off the machine.
 
 def ask(resources):
-    choose = input("What would you like? (espresso/latte/cappuccino): ")
-    water_add = ["water"]
-    milk_add = ["milk"]
-    coffee_add = ["coffee"]
-    coins = ""
+    choose=input("What would you like? (espresso/latte/cappuccino): ")
+    water_add=["water"]
+    milk_add=["milk"]
+    coffee_add=["coffee"]
+    coins=""
 
-    if choose == "report":
+    if choose=="report":
         return (f"{water_add},{milk_add},{coffee_add}")
-    elif choose == "espresso":
+    elif choose=="espresso":
         count()
-    elif choose == "latte":
+    elif choose=="latte":
         count()
-
 
 ask(resources)
 
-# TODO-2: Turn Off the Machine:
+#TODO-2: Turn Off the Machine:
 #     If the user enters "off", terminate the program.
 #
-# TODO-3: Generate a Report**:
+#TODO-3: Generate a Report**:
 #     Implement a function to print the current status of resources (water, milk, coffee, money).
 #
-# TODO-4: Check Resources:
+#TODO-4: Check Resources:
 #    Add a function to check if there are enough resources to make the selected drink.
 #    If not enough resources, print: "Sorry there is not enough [resource].".
 
-# TODO-7: Make the Coffee:
+#TODO-7: Make the Coffee:
 #    Implement a function to:
 #    Deduct the required resources from the machine.
 #    Print: "Here is your [drink]. Enjoy!".
 #
-# TODO-8: Main Program Loop:
+#TODO-8: Main Program Loop:
 #    Organize the logic into a continuous loop until the user enters "off".
 #    Repeat the prompt after each action (e.g., making a drink or showing a report).
 #
-# TODO-9: Data Structure:
+#TODO-9: Data Structure:
 #    Store resources and drink recipes in a structured format, like a dictionary.
 
 
